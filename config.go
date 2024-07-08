3078705489f7b5667daf6784c984c129b8d01faa
Damon <damon@damongolding.com>
2024-07-08 15:13:46 +0100

    config overrides via url params

---
+++ config.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"net/url"
+	"os"
+	"reflect"
+	"strconv"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Refresh    int    `yaml:"refresh"`
+	People     string `yaml:"people"`
+	Album      string `yaml:"album"`
+	FillScreen bool   `yaml:"fill_screen"`
+}
+
+// Load loads config file
+func (c *Config) Load() error {
+	config := Config{
+		Refresh:    20,
+		FillScreen: true,
+	}
+
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return err
+	}
+
+	*c = config
+
+	return nil
+}
+
+func (c *Config) ConfigWithOverrides(queries url.Values) Config {
+
+	configWithOverrides := c
+
+	v := reflect.ValueOf(configWithOverrides).Elem()
+
+	// Loop through the map and update struct fields
+	for key, values := range queries {
+		if len(values) > 0 {
+			// Lets just use the first used override
+			value := values[0]
+			if value == "" {
+				continue
+			}
+
+			key = cases.Title(language.English, cases.Compact).String(key)
+
+			// Get the field by name
+			field := v.FieldByName(key)
+			if field.IsValid() && field.CanSet() {
+				// Set field (covert to correct type if needed)
+				switch field.Kind() {
+				case reflect.String:
+					field.SetString(value)
+				case reflect.Int:
+					if n, err := strconv.Atoi(value); err == nil {
+						field.SetInt(int64(n))
+					}
+				}
+
+			}
+		}
+	}
+
+	return *configWithOverrides
+}
