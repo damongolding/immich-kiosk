8537a096ccee3a1b1dce7999a76e052dc3d232f1
Damon <damon@damongolding.com>
2024-09-05 11:28:20 +0100

    remove visible input on first load

--- views/home_templ.go
+++ views/home_templ.go
@@ -356,7 +356,7 @@ func historyForm() templ.Component {
 			templ_7745c5c3_Var10 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form id=\"kiosk-history\" hx-swap-oob=\"true\"><input class=\"kiosk-history--entry\" value=\"\"></form>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form id=\"kiosk-history\" hx-swap-oob=\"true\"><input class=\"kiosk-history--entry\" type=\"hidden\" value=\"\"></form>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
