5f867a0efef66daa85b3294df7e4e0358adcb4a4
Damon <2184238+damongolding@users.noreply.github.com>
2024-12-09 10:19:10 +0000

    more http settings

--- internal/immich/immich.go
+++ internal/immich/immich.go
@@ -6,6 +6,7 @@
 package immich
 
 import (
+	"net"
 	"net/http"
 	"sync"
 	"time"
@@ -52,6 +53,13 @@ var (
 		IdleConnTimeout:     90 * time.Second,
 		DisableKeepAlives:   false,
 		MaxIdleConnsPerHost: 100,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 30 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	}
 	// httpClient default http client for Immich api calls
 	httpClient = &http.Client{
