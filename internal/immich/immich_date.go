7a17de2c251a218ece9ad3d8c051527450e5f280
Damon <2184238+damongolding@users.noreply.github.com>
2025-01-03 21:57:38 +0000

    moved date filter to own file

--- internal/immich/immich_date.go
+++ internal/immich/immich_date.go
@@ -5,19 +5,55 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/google/go-querystring/query"
 	"github.com/patrickmn/go-cache"
 )
 
-// GetRandomImage retrieve a random image from Immich
-func (i *ImmichAsset) RandomImage(requestID, kioskDeviceID string, isPrefetch bool) error {
+// RandomImageInDateRange gets a random image from the Immich server within the specified date range.
+// dateRange should be in the format "YYYY-MM-DD_to_YYYY-MM-DD"
+// requestID is used for tracing the request through logs
+// kioskDeviceID identifies the kiosk device making the request
+// isPrefetch indicates if this is a prefetch request
+func (i *ImmichAsset) RandomImageInDateRange(dateRange, requestID, kioskDeviceID string, isPrefetch bool) error {
+	return i.randomImageInDateRange(dateRange, requestID, kioskDeviceID, isPrefetch, 0)
+}
+
+// randomImageInDateRange is the internal implementation of RandomImageInDateRange that includes retry logic
+// It makes requests to the Immich API to get random images within the date range and handles caching
+// retries tracks the number of retry attempts made
+func (i *ImmichAsset) randomImageInDateRange(dateRange, requestID, kioskDeviceID string, isPrefetch bool, retires int) error {
+
+	if retires >= MaxRetries {
+		return fmt.Errorf("No images found for '%s'. Max retries reached.", dateRange)
+	}
+
+	dates := strings.Split(dateRange, "_")
+	dateStart, err := time.Parse("2006-01-02", dates[0])
+	if err != nil {
+		return err
+	}
+	dateEnd, err := time.Parse("2006-01-02", dates[2])
+	if err != nil {
+		return err
+	}
+
+	if dateEnd.Before(dateStart) {
+		dateStart, dateEnd = dateEnd, dateStart
+	}
+
+	dateStartHuman := dateStart.Format("2006-01-02")
+	dateEndHuman := dateEnd.Format("2006-01-02")
+
+	dateEnd = dateEnd.Add(time.Hour * 23).Add(time.Minute * 59).Add(time.Second * 59)
 
 	if isPrefetch {
-		log.Debug(requestID, "PREFETCH", kioskDeviceID, "Getting Random image", true)
+		log.Debug(requestID, "PREFETCH", kioskDeviceID, "Getting Random image from", dateStartHuman, "to", dateEndHuman)
 	} else {
-		log.Debug(requestID + " Getting Random image")
+		log.Debug(requestID+" Getting Random image", "from", dateStartHuman, "to", dateEndHuman)
 	}
 
 	var immichAssets []ImmichAsset
@@ -29,10 +65,12 @@ func (i *ImmichAsset) RandomImage(requestID, kioskDeviceID string, isPrefetch bo
 	}
 
 	requestBody := ImmichSearchRandomBody{
-		Type:       string(ImageType),
-		WithExif:   true,
-		WithPeople: true,
-		Size:       requestConfig.Kiosk.FetchedAssetsSize,
+		Type:        string(ImageType),
+		TakenAfter:  dateStart.Format(time.RFC3339),
+		TakenBefore: dateEnd.Format(time.RFC3339),
+		WithExif:    true,
+		WithPeople:  true,
+		Size:        requestConfig.Kiosk.FetchedAssetsSize,
 	}
 
 	if requestConfig.ShowArchived {
@@ -70,7 +108,8 @@ func (i *ImmichAsset) RandomImage(requestID, kioskDeviceID string, isPrefetch bo
 	if len(immichAssets) == 0 {
 		log.Debug(requestID + " No images left in cache. Refreshing and trying again")
 		apiCache.Delete(apiUrl.String())
-		return i.RandomImage(requestID, kioskDeviceID, isPrefetch)
+		retires++
+		return i.randomImageInDateRange(dateRange, requestID, kioskDeviceID, isPrefetch, retires)
 	}
 
 	for immichAssetIndex, img := range immichAssets {
@@ -95,11 +134,15 @@ func (i *ImmichAsset) RandomImage(requestID, kioskDeviceID string, isPrefetch bo
 			}
 		}
 
+		img.KioskSourceName = fmt.Sprintf("%s to %s", dateStartHuman, dateEndHuman)
+
 		*i = img
+
 		return nil
 	}
 
 	log.Debug(requestID + " No viable images left in cache. Refreshing and trying again")
 	apiCache.Delete(apiUrl.String())
-	return i.RandomImage(requestID, kioskDeviceID, isPrefetch)
+	retires++
+	return i.randomImageInDateRange(dateRange, requestID, kioskDeviceID, isPrefetch, retires)
 }
