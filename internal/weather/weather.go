d44e7c953c520281e8a22e65c902603106c5b5d6
Damon <2184238+damongolding@users.noreply.github.com>
2025-04-11 10:07:06 +0100

    lowercase for matching wetaher name

--- internal/weather/weather.go
+++ internal/weather/weather.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"sync"
 	"time"
 
@@ -127,7 +128,7 @@ func AddWeatherLocation(ctx context.Context, location config.WeatherLocation) {
 		Lang: location.Lang,
 	}
 
-	weatherDataStore.Store(w.Name, *w)
+	weatherDataStore.Store(strings.ToLower(w.Name), *w)
 
 	// Run once immediately
 	log.Debug("Getting initial weather for", "name", w.Name)
@@ -135,7 +136,7 @@ func AddWeatherLocation(ctx context.Context, location config.WeatherLocation) {
 	if newWeatherInitErr != nil {
 		log.Error("Failed to update initial weather", "name", w.Name, "error", newWeatherInitErr)
 	} else {
-		weatherDataStore.Store(w.Name, newWeatherInit)
+		weatherDataStore.Store(strings.ToLower(w.Name), newWeatherInit)
 		log.Debug("Retrieved initial weather for", "name", w.Name)
 	}
 
@@ -151,7 +152,7 @@ func AddWeatherLocation(ctx context.Context, location config.WeatherLocation) {
 				log.Error("Failed to update weather", "name", w.Name, "error", newWeatherErr)
 				continue
 			}
-			weatherDataStore.Store(w.Name, newWeather)
+			weatherDataStore.Store(strings.ToLower(w.Name), newWeather)
 			log.Debug("Retrieved weather for", "name", w.Name)
 		}
 	}
@@ -160,7 +161,7 @@ func AddWeatherLocation(ctx context.Context, location config.WeatherLocation) {
 // CurrentWeather retrieves the current weather data for a given location name.
 // Returns a WeatherLocation struct containing the weather data, or an empty struct if not found.
 func CurrentWeather(name string) Location {
-	value, ok := weatherDataStore.Load(name)
+	value, ok := weatherDataStore.Load(strings.ToLower(name))
 	if !ok {
 		return Location{}
 	}
