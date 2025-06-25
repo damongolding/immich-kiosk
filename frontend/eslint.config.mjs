57002077bd11cf806a05ba3851e4e522741b9435
Damon <2184238+damongolding@users.noreply.github.com>
2025-06-25 10:01:38 +0100

    spaces

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,19 +2,19 @@ import compat from "eslint-plugin-compat";
 import globals from "globals";
 
 export default [
-	{
-		ignores: ["**/*.ts"],
-		plugins: {
-			compat,
-		},
+  {
+    ignores: ["**/*.ts"],
+    plugins: {
+      compat,
+    },
 
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-		},
-		rules: {
-			"compat/compat": "warn",
-		},
-	},
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "compat/compat": "warn",
+    },
+  },
 ];
