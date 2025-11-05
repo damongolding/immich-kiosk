59779ee7bd43721a281bb69735c405f1ee5e9f8c
Damon <2184238+damongolding@users.noreply.github.com>
2025-11-05 11:45:39 +0000

    skip sub 1 sec video

--- internal/immich/immich_video.go
+++ internal/immich/immich_video.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"net/url"
 	"path"
+	"time"
+
+	"github.com/charmbracelet/log"
 )
 
 // Video retrieves the video asset from Immich server.
@@ -33,5 +36,24 @@ func (a *Asset) Video() ([]byte, string, error) {
 	}
 
 	return responseBody, contentType, nil
+}
 
+// durationCheck verifies that the video duration string in the Asset is valid and represents
+// a duration of at least one second.
+//
+// Returns true if the duration is valid and at least one second, false otherwise.
+//
+// The duration string is expected to be in the format "HH:MM:SS".
+func (a *Asset) durationCheck() bool {
+
+	d, err := time.Parse("15:04:05", a.Duration)
+	if err != nil {
+		log.Error("Failed to parse video duration", "ID", a.ID, "duration", a.Duration)
+		return false
+	}
+	if d.Hour() == 0 && d.Minute() == 0 && d.Second() < 1 {
+		return false
+	}
+
+	return true
 }
