91247d56087890326dcdd46d0784736ebd2ec4b3
Damon <2184238+damongolding@users.noreply.github.com>
2026-02-12 16:13:32 +0000

    better guards

--- internal/weather/weather.go
+++ internal/weather/weather.go
@@ -66,7 +66,7 @@ func (s *LocationRotate) Get(i int) string {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	if len(s.items) == 0 || i < 0 {
+	if len(s.items) == 0 || i >= len(s.items) || i < 0 {
 		return ""
 	}
 
@@ -77,7 +77,7 @@ func (s *LocationRotate) Next(i int) (int, string) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	if len(s.items) == 0 || i < 0 {
+	if len(s.items) == 0 || i >= len(s.items) || i < 0 {
 		return 0, ""
 	}
 
