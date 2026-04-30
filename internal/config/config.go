6ae3d6f4ef4f2d40bd95bc3aae607922e70b9150
Damon <2184238+damongolding@users.noreply.github.com>
2026-04-30 12:15:23 +0100

    add the filter

--- internal/config/config.go
+++ internal/config/config.go
@@ -344,7 +344,8 @@ type Config struct {
 	// FilterDate filter certain asset bucket assets by date range
 	FilterDate string `json:"filterDate" yaml:"filter_date" mapstructure:"filter_date" query:"filter_date" form:"filter_date" default:""`
 	// FilterNewest filter certain asset bucket assets by the newest X assets
-	FilterNewest int `json:"filterNewest" yaml:"filter_newest" mapstructure:"filter_newest" query:"filter_newest" form:"filter_newest" default:"0"`
+	FilterNewest  int  `json:"filterNewest" yaml:"filter_newest" mapstructure:"filter_newest" query:"filter_newest" form:"filter_newest" default:"0"`
+	FilterNoFaces bool `json:"filterNoFaces" yaml:"filter_no_faces" mapstructure:"filter_no_faces" query:"filter_no_faces" form:"filter_no_faces" default:"false"`
 
 	// ShowClearCacheButton display a button to clear cache
 	ShowClearCacheButton bool `json:"showClearCacheButton" yaml:"show_clear_cache_button" mapstructure:"show_clear_cache_button" query:"show_clear_cache_button" form:"show_clear_cache_button" default:"false"`
