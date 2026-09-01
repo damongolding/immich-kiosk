61702e6103345f6542beddbca1d601e0359d1ec7
Damon <2184238+damongolding@users.noreply.github.com>
2026-09-01 09:41:32 +0100

    delay album laoding

--- internal/routes/routes_redirect.go
+++ internal/routes/routes_redirect.go
@@ -10,6 +10,7 @@ import (
 	"github.com/damongolding/immich-kiosk/internal/common"
 	"github.com/damongolding/immich-kiosk/internal/config"
 	"github.com/damongolding/immich-kiosk/internal/kiosk"
+	"github.com/damongolding/immich-kiosk/internal/templates/partials"
 	"github.com/damongolding/immich-kiosk/internal/utils"
 
 	"github.com/labstack/echo/v5"
@@ -133,4 +134,29 @@ func mergeRequestQueries(requestQueries url.Values, redirectItem config.Redirect
 	redirectItem.URL = redirectURL.String()
 
 	return redirectItem
+}
+
+func AlbumRedirects(baseConfig *config.Config, com *common.Common) echo.HandlerFunc {
+	return func(c *echo.Context) error {
+		requestData, err := InitializeRequestData(c, baseConfig)
+		if err != nil {
+			return err
+		}
+
+		if requestData == nil {
+			log.Info("Refreshing clients")
+			return nil
+		}
+
+		requestConfig := requestData.RequestConfig
+		requestID := requestData.RequestID
+
+		log.Debug(
+			requestID,
+			"method", c.Request().Method,
+			"path", c.Request().URL.String(),
+		)
+
+		return Render(c, http.StatusOK, partials.AlbumRedirectsFragment(requestConfig, c.QueryParams(), com.Context()))
+	}
 }
