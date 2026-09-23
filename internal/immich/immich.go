8e03ccf4b233cf6e4c4c55e3000e32444a55191e
damongolding <damon@damongolding.com>
2026-09-23 21:28:08 +0100

    fmt

--- internal/immich/immich.go
+++ internal/immich/immich.go
@@ -481,9 +481,9 @@ func (b *SearchFilter) ExcludeAlbums(a []string) *SearchFilter {
 
 // TODO: These need to be tag IDs not value(s). Disabling for now
 func (b *SearchFilter) ExcludeTags(t []string) *SearchFilter {
-	//if len(t) > 0 {
-	//	b.TagIDs.None = t
-	//}
+	// if len(t) > 0 {
+	// 	b.TagIDs.None = t
+	// }
 	return b
 }
 
