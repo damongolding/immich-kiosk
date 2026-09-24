c4304a99f2744378f2600592df71dc7fe4ad2232
Damon <2184238+damongolding@users.noreply.github.com>
2026-09-24 08:48:16 +0100

    remove logging and fmt

--- internal/immich/immich_helpers.go
+++ internal/immich/immich_helpers.go
@@ -981,8 +981,6 @@ func (a *Asset) fetchPaginatedMetadataWithCache(u *url.URL, requestBody SearchRa
 		return res, nil
 	}
 
-	log.Info("fetchPaginatedMetadataWithCache", "cursor", requestBody.Cursor, "album(s)", requestBody.Filter.AlbumIDs, "api", a.requestConfig.ImmichAPIKey)
-
 	fetcher := New(a.ctx, a.requestConfig)
 
 	requestBody.Cursor = nextCursor
@@ -1007,8 +1005,6 @@ func (a *Asset) backfillPaginatedMetadata(u *url.URL, requestBody SearchRandomBo
 			log.Warn("reached maximum page count when backfilling Metadata")
 			break
 		}
-
-		log.Info("fetchMetadataPage", "page", page, "cursor", requestBody.Cursor, "album(s)", requestBody.Filter.AlbumIDs, "api", a.requestConfig.ImmichAPIKey)
 
 		pageAssets, nextCursor, err := a.fetchMetadataPage(a.ctx, u, requestBody, requestID, deviceID)
 		if err != nil {
